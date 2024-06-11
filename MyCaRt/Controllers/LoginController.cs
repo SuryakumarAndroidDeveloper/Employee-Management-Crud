c54dde68271c02cdb403610245b6a850d628e321
suryakumar <suryakumar@TISRNT03>
2024-06-11 11:22:54 +0530

    design commit

---
+++ MyCaRt/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCaRt.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login_Register()
+        {
+            return View();
+        }
+    }
+}
